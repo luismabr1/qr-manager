2cece3a8d07ba53d84b9b6beea0644defdc7b0e4
Luis Manuel Brito <lsbrt4@gmail.com>
2024-05-31 15:39:17 -0400

    No encontre el error con el typo: se los quite

--- src/app/context/authprovider.js
+++ src/app/context/authprovider.js
@@ -4,8 +4,6 @@ import { SessionProvider } from "next-auth/react";
 
 export default function AuthProvider({
   children,
-}: {
-  children: React.ReactNode;
 }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,16 @@
 // Layout.js
 import Navbar from "./Navbar";
 
+
 export default function Layout({ children, session }) {
   return (
     <>
       <Navbar session={session} /> {/* Pasa la sesión al componente Navbar */}
-      {children}
+      <main>{children}</main>
     </>
   );
 }
+
+
 
 
