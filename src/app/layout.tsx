9200ef9aa884d5e318490215e9017c07b7a900bd
Luis Manuel Brito <lsbrt4@gmail.com>
2024-06-05 15:33:16 -0400

    agrego el componente el insights de vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 import AuthProvider from "./context/authprovider";
 import Layout from "../components/Layout";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
               {children}
           </Layout>
           <Analytics />
+          <SpeedInsights/>
         </AuthProvider>
       </body>
     </html>
